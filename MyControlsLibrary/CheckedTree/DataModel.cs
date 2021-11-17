52221ce388adb906dfb3d74b6cb3f9272879ef2c
Kazimir <avvlasov@outlook.com>
2021-11-17 06:27:33 +0300

    Добавьте файлы проекта.

---
+++ MyControlsLibrary/CheckedTree/DataModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace MyControlsLibrary.CheckedTree;
+public class TreeFamily : DependencyObject, IParent<object>
+{
+    public string? Name { get; set; }
+    public int Id { get; set; }
+    public List<TreePerson>? Members { get; set; }
+
+    IEnumerable<object>? IParent<object>.GetChildren()
+    {
+        return Members;
+    }
+}
+
+public class TreePerson : DependencyObject
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ MyControlsLibrary/CheckedTree/IParent.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace MyControlsLibrary.CheckedTree;
+interface IParent<T>
+{
+    IEnumerable<T> GetChildren();
+}
